79911e269300cde342d93c61f037264b9f52b188
Hughes <codedpiano@gmail.com>
2022-09-30 22:09:18 -0400

    misc changes for deploying

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import SingleQuant from './SingleQuant';
 import ErrorPage from './ErrorPage';
 import Home from './Home';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: "/",
         element: <Home />,
